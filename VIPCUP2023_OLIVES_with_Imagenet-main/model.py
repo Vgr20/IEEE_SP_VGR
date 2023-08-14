76a94d8ac696e38cff0fc0571b3c5f1ab394dc25
Vishagar Arunan <vishagara.20@uom.lk>
2023-08-14 18:59:36 +0530

    Final

--- VIPCUP2023_OLIVES_with_Imagenet-main/model.py
+++ VIPCUP2023_OLIVES_with_Imagenet-main/model.py
@@ -13,6 +13,11 @@ class ResNet(nn.Module):
             self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
             self.encoder.fc = nn.Identity()
             self.fc = nn.Linear(2048, num_classes)
+        elif (name == 'resnet152'):
+            self.encoder = torchvision.models.resnet152(zero_init_residual=True)
+            self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+            self.encoder.fc = nn.Identity()
+            self.fc = nn.Linear(2048, num_classes)
         else:
             self.encoder = torchvision.models.resnet18(zero_init_residual=True)
             self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
